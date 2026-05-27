47908da25af6daa93dfe60a18e2b93c81323f55a
Mark Jayson P. Morales <mjpmorales1117@gmail.com>
2026-05-28 04:38:22 +0800

    Update cards.js

--- javascript-card-game/game/cards.js
+++ javascript-card-game/game/cards.js
@@ -7,10 +7,22 @@ export const cardTypes = {
 };
 
 export const cards = [
-  { name: "Rusty Blade", type: cardTypes.ATTACK, effect: (self, opp) => opp.health -= 1 },
-  { name: "Scavenged Rifle", type: cardTypes.ATTACK, effect: (self, opp) => opp.health -= 2 },
-  { name: "Makeshift Armor", type: cardTypes.DEFENSE, effect: (self) => self.block = 1 },
-  { name: "Water Cache", type: cardTypes.RESOURCE, effect: (self) => self.resources += 2 },
-  { name: "Supply Raid", type: cardTypes.RAID, effect: (self, opp) => { opp.resources -= 2; self.resources += 2; } },
-  { name: "Raiders Alliance", type: cardTypes.ALLIANCE, cost: 3, effect: (self, opp) => { if (self.resources >= 3) { self.resources -= 3; opp.health -= 4; } } }
+  { name: "Rusty Blade", type: cardTypes.ATTACK, rarity: "common", effect: (self, opp) => opp.health -= 1 },
+  { name: "Scavenged Rifle", type: cardTypes.ATTACK, rarity: "uncommon", effect: (self, opp) => opp.health -= 2 },
+  { name: "Makeshift Armor", type: cardTypes.DEFENSE, rarity: "common", effect: (self) => self.block = 1 },
+  { name: "Water Cache", type: cardTypes.RESOURCE, rarity: "common", effect: (self) => self.resources += 2 },
+  { name: "Supply Raid", type: cardTypes.RAID, rarity: "uncommon", effect: (self, opp) => { opp.resources -= 2; self.resources += 2; } },
+  { name: "Raiders Alliance", type: cardTypes.ALLIANCE, rarity: "rare", cost: 3, effect: (self, opp) => { if (self.resources >= 3) { self.resources -= 3; opp.health -= 4; } } }
 ];
+
+function buildDeck(cardPool) {
+  let deck = [];
+  cardPool.forEach(card => {
+    if (card.rarity === "common") deck.push(...Array(4).fill(card));
+    else if (card.rarity === "uncommon") deck.push(...Array(2).fill(card));
+    else if (card.rarity === "rare") deck.push(card);
+  });
+  return deck;
+}
+
+
